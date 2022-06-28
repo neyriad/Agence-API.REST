7f12dd4ddbe26b20d7ffe3e5d41d66774636aad4
Dayrien Corrales Díaz <neyriad@gmail.com>
2022-06-28 16:43:21 -0400

    Added controllers

---
+++ Agence-API.REST/Controllers/PerformanceController.cs
@@ -0,0 +1,35 @@
+﻿using Agence_API.REST.Application;
+using Agence_API.REST.Domain.EntitiesDto;
+using Agence_API.REST.Domain.Helpers;
+using Agence_API.REST.Domain.ModelView;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Agence_API.REST.Controllers
+{
+    [RoutePrefix("api/performance")]
+    public class PerformanceController : ApiController
+    {
+        [HttpGet]
+        [Route("consultants")]
+        public List<ConsultantDto> GetAllConsultores(SearchParamsDto options)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.BadRequest, 
+                    ModelState));
+
+            try
+            {
+                return PerformanceApplication.GetAllConsultants(options);
+            }
+            catch (Exception ex)
+            {
+                throw ExceptionBuilder.Create(HttpStatusCode.NotFound, "Error getting all users", 
+                    $"Details: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Agence-API.REST/Controllers/SalaryController.cs
@@ -0,0 +1,30 @@
+﻿using Agence_API.REST.Application;
+using Agence_API.REST.Domain.EntitiesDto;
+using Agence_API.REST.Domain.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+
+namespace Agence_API.REST.Controllers
+{
+    [RoutePrefix("api/salary")]
+    public class SalaryController : ApiController
+    {
+        // GET api/salarios
+        [HttpGet]
+        public List<SalaryDto> GetAll()
+        {
+            try
+            {
+                var result = SalaryApplication.GetAll();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ExceptionBuilder.Create(HttpStatusCode.NotFound, "Error getting all salary", 
+                    $"Details: {ex.Message}");
+            }
+        }
+    }
+}
