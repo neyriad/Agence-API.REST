503665e35c6caaca2256125b6a4be1fa13e87dfa
Dayrien Corrales Díaz <neyriad@gmail.com>
2022-06-28 16:43:15 -0400

    Added application handlers

---
+++ Agence-API.REST/Application/PerformanceApplication.cs
@@ -0,0 +1,19 @@
+﻿using Agence_API.REST.Domain.EntitiesDto;
+using Agence_API.REST.Domain.ModelView;
+using System.Collections.Generic;
+
+namespace Agence_API.REST.Application
+{
+    public static class PerformanceApplication
+    {
+        /// <summary>
+        /// Responsible of getting all consultants
+        /// </summary>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public static List<ConsultantDto> GetAllConsultants(SearchParamsDto options)
+        {
+            return null;
+        }
+    }
+}
---
+++ Agence-API.REST/Application/SalaryApplication.cs
@@ -0,0 +1,16 @@
+﻿using Agence_API.REST.Domain.DataAccess;
+using Agence_API.REST.Domain.EntitiesDto;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Agence_API.REST.Application
+{
+    public static class SalaryApplication
+    {
+        public static List<SalaryDto> GetAll()
+        {
+            var list = EntitiesDataAccess.Instance.cao_salario.ToList();
+            return list.Select(s => SalaryDto.ToBusinessEntity(s)).ToList();
+        }
+    }
+}
