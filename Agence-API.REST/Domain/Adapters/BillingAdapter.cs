c256ae3c6541394652cc2bbe66ae0be46322d0d4
Dayrien Corrales Díaz <neyriad@gmail.com>
2022-06-30 17:59:41 -0400

    Added adapters

---
+++ Agence-API.REST/Domain/Adapters/BillingAdapter.cs
@@ -0,0 +1,45 @@
+﻿using Agence_API.REST.Domain.EntitiesDto;
+using System.Data.Common;
+
+namespace Agence_API.REST.Domain.Adapters
+{
+    public static class BillingAdapter
+    {
+        public static BillingDto BusinessEntityForEarnings(DbDataReader entity)
+        {
+            if (null == entity) return null;
+
+            return new BillingDto
+            {
+                BillId = entity.IsDBNull(entity.GetOrdinal("BillId"))
+                                    ? default : entity.GetInt32(entity.GetOrdinal("BillId")),
+
+                BillValue = entity.IsDBNull(entity.GetOrdinal("BillValue"))
+                                    ? default : entity.GetDouble(entity.GetOrdinal("BillValue")),
+
+                Commission = entity.IsDBNull(entity.GetOrdinal("Commission"))
+                                    ? default : entity.GetDouble(entity.GetOrdinal("Commission")),
+
+                Taxes = entity.IsDBNull(entity.GetOrdinal("Taxes"))
+                                    ? default : entity.GetDouble(entity.GetOrdinal("Taxes"))
+            };
+        }
+
+        public static BillingDto BusinessEntityForPerformance(DbDataReader entity)
+        {
+            if (null == entity) return null;
+
+            return new BillingDto
+            {
+                BillValue = entity.IsDBNull(entity.GetOrdinal("BillValue"))
+                                    ? default : entity.GetDouble(entity.GetOrdinal("BillValue")),
+
+                BillDate = entity.IsDBNull(entity.GetOrdinal("BillDate"))
+                                    ? default : entity.GetDateTime(entity.GetOrdinal("BillDate")),
+
+                Taxes = entity.IsDBNull(entity.GetOrdinal("Taxes"))
+                                    ? default : entity.GetDouble(entity.GetOrdinal("Taxes"))
+            };
+        }
+    }
+}
---
+++ Agence-API.REST/Domain/Adapters/ConsultantAdapter.cs
@@ -0,0 +1,31 @@
+﻿using Agence_API.REST.Domain.EntitiesDto;
+using System.Data.Common;
+
+namespace Agence_API.REST.Domain.Adapters
+{
+    public static class ConsultantAdapter
+    {
+        public static ConsultantDto ToBusinessEntity(DbDataReader entity)
+        {
+            if (null == entity) return null;
+
+            return new ConsultantDto
+            {
+                Name = entity.IsDBNull(entity.GetOrdinal("Name"))
+                                    ? default : entity.GetString(entity.GetOrdinal("Name")),
+
+                EmailJob = entity.IsDBNull(entity.GetOrdinal("EmailJob"))
+                                    ? default : entity.GetString(entity.GetOrdinal("EmailJob")),
+
+                EmailPrivate = entity.IsDBNull(entity.GetOrdinal("EmailPrivate"))
+                                    ? default : entity.GetString(entity.GetOrdinal("EmailPrivate")),
+
+                Phone = entity.IsDBNull(entity.GetOrdinal("Phone"))
+                                    ? default : entity.GetString(entity.GetOrdinal("Phone")),
+
+                UserName = entity.IsDBNull(entity.GetOrdinal("UserName"))
+                                    ? default : entity.GetString(entity.GetOrdinal("UserName"))
+            };
+        }
+    }
+}
