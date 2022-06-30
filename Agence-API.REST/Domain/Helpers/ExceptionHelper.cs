94f3b9d758e019efbc4427a07d98427b8025c7a3
Dayrien Corrales Díaz <neyriad@gmail.com>
2022-06-30 17:58:56 -0400

    Updated helpers name

--- Agence-API.REST/Domain/Helpers/ExceptionHelper.cs
+++ Agence-API.REST/Domain/Helpers/ExceptionHelper.cs
@@ -4,7 +4,7 @@ using System.Web.Http;
 
 namespace Agence_API.REST.Domain.Helpers
 {
-    public static class ExceptionBuilder
+    public static class ExceptionHelper
     {
         public static HttpResponseException Create(HttpStatusCode code, string reason, string content)
         {
